2ef6b6ef1183932ce494ee2a8018096151f35a55
shubhendu21 <shivam21021992@gmail.com>
2025-05-09 15:51:57 +0530

    Create qa-main.tf

---
+++ terraform/qa-main.tf
@@ -0,0 +1,27 @@
+module "develop" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "shivam21021992+qa@gmail.com"
+    AccountName               = "qa-main"
+    ManagedOrganizationalUnit = "Infrastructure"
+    SSOUserEmail              = "shivam21021992+qa@gmail.com"
+    SSOUserFirstName          = "qa"
+    SSOUserLastName           = "main"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "none"
+}
